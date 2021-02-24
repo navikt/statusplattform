0f539bc79a4cece9a4d9504280574c6f37ce1d20
Andreas Nordahl <andnorda@gmail.com>
2021-02-24 16:32:02 +0100

    refactor next.config.js some

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,18 @@
+const withPlugins = require('next-compose-plugins')
+const withTranspileModules = require('next-transpile-modules')
 const withCss = require('@zeit/next-css')
 const withLess = require('@zeit/next-less')
 const packageJson = require('./package.json')
-const withPlugins = require('next-compose-plugins')
-const navFrontendModuler = []
-Object.keys(packageJson.dependencies).forEach((key) => {
-  if (key.startsWith('nav-frontend-')) {
-    navFrontendModuler.push(key)
-  }
-})
-const withTranspileModules = require('next-transpile-modules')(
-  navFrontendModuler,
+
+module.exports = withPlugins(
+  [
+    withTranspileModules(
+      Object.keys(packageJson.dependencies).filter((key) =>
+        key.startsWith('nav-frontend-'),
+      ),
+    ),
+    withCss,
+    withLess,
+  ],
+  {},
 )
-module.exports = withPlugins([withTranspileModules, withCss, withLess], {})
