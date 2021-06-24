11dd739e12ded1d4c5214c9228d7d31532f0a96e
Christian-Haugland <christian.rudfoss.haugland@nav.no>
2021-06-24 13:19:43 +0200

    Kobles mot labs-cluster for å få det eksternt tilgjengelig

--- portal-infrastructure/src/main/java/no/nav/portal/infrastructure/CORSFilter.java
+++ portal-infrastructure/src/main/java/no/nav/portal/infrastructure/CORSFilter.java
@@ -44,12 +44,15 @@ public class CORSFilter implements Filter {
         // Authorize (allow) all domains to consume the content
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers", "*");
+
 
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
 
         // For HTTP OPTIONS verb/method reply with ACCEPTED status code -- per CORS handshake
         if (request.getMethod().equals("OPTIONS")) {
             resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+            ((HttpServletResponse) servletResponse).addHeader("Access-Control-max-age", "600");
             return;
         }
 
