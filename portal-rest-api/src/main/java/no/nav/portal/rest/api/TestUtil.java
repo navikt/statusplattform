93227d8930426471ad20aabcd721e041022cea2f
larsLotNav <lars.august.lotsberg@nav.no>
2021-07-08 12:35:23 +0200

    Lagt til mer testdata

--- portal-rest-api/src/main/java/no/nav/portal/rest/api/TestUtil.java
+++ portal-rest-api/src/main/java/no/nav/portal/rest/api/TestUtil.java
@@ -20,18 +20,44 @@ public class TestUtil {
                 .beskrivelse("Pensjon ...").id("PENS").rangering(2));
         DEFAULT_AREAS.put("BARN",new AreaDto().name("Venter barn")
                 .beskrivelse("Venter barn  ...").id("BARN").rangering(3));
+
+        DEFAULT_AREAS.put("ALBR",new AreaDto().name("Alene med barn")
+                .beskrivelse("Venter barn  ...").id("ALBR").rangering(4));
+        DEFAULT_AREAS.put("SYKM",new AreaDto().name("Sykmeldt")
+                .beskrivelse("Sykmeldt ...").id("SYKM").rangering(5));
+        DEFAULT_AREAS.put("SAAP",new AreaDto().name("Skal søke AAP")
+                .beskrivelse("Skal søke AAP ...").id("SAAP").rangering(6));
+
+        DEFAULT_AREAS.put("SYKF",new AreaDto().name("Sykdom i familien")
+                .beskrivelse("Sykdom i familien  ...").id("SYKF").rangering(7));
+        DEFAULT_AREAS.put("TILR",new AreaDto().name("Trenger tilrettelegging")
+                .beskrivelse("Trenger tilrettelegging ...").id("TILR").rangering(8));
+        DEFAULT_AREAS.put("OKOH",new AreaDto().name("Trenger økonomisk sosialhjelp")
+                .beskrivelse("Trenger økonomisk sosialhjelp...").id("OKOH").rangering(9));
+
+        DEFAULT_AREAS.put("OKHJ",new AreaDto().name("Trenger økonomisk rådgiving")
+                .beskrivelse("Trenger økonomisk rådgiving ...").id("OKHJ").rangering(10));
+        DEFAULT_AREAS.put("BEOS",new AreaDto().name("Berørt av EØS-saken")
+                .beskrivelse("Berørt av EØS-saken ...").id("BEOS").rangering(11));
+        DEFAULT_AREAS.put("MNIF",new AreaDto().name("Har mistet noen i nær familie")
+                .beskrivelse("Har mistet noen i nær familie  ...").id("MNIF").rangering(12));
+
         //Legg til område/area her
         AREAS_INSTANCES.put(DEFAULT_AREAS.get("ARBD"), Arrays.asList("Arena","Meldekort", "Oppf. arbeid", "Søk Dagpenger"));
         AREAS_INSTANCES.put(DEFAULT_AREAS.get("PENS"), Arrays.asList("Sykmelding","Frikort", "eResept. arbeid", "Søk Dagpenger"));
         AREAS_INSTANCES.put(DEFAULT_AREAS.get("BARN"), Arrays.asList("Bidrag","Engangsstønad", "Foreldrepenger"));
-        /*AREAS_INSTANCES.put("Alene med barn", Arrays.asList("Din Pensjon","Pesys"));
-        AREAS_INSTANCES.put("Sykmeldt", Arrays.asList("Dine Utbetalinger","DittNAV", "nav.no", "Søknadsdialoger"));
-        AREAS_INSTANCES.put("Skal søke AAP", Arrays.asList("Dokumentløsning","Gosys", "Modiabrukerdialog", "Navet","Personkortet","WinTid"));
-        AREAS_INSTANCES.put("Har sykdom i familien", Arrays.asList("Ok"));
-        AREAS_INSTANCES.put("Trenger tilrettelegging", Arrays.asList("AAregister","AktørRegister", "Datavarehus","DKIF", "Enhetsregister","Henvendelse", "Inntektskomponenten","Joark","NORG","TPS","TSS"));
-        AREAS_INSTANCES.put("Trenger økonomisk sosialhjelp", Arrays.asList("BankID-status","Google Cloud-status", "IDporten", "MS Azure-status"));
-    */
 
+        AREAS_INSTANCES.put(DEFAULT_AREAS.get("ALBR"), Arrays.asList("Din Pensjon","Pesys"));
+        AREAS_INSTANCES.put(DEFAULT_AREAS.get("SYKM"), Arrays.asList("BankID-status","Google Cloud-status", "IDporten", "MS Azure-status"));
+        AREAS_INSTANCES.put(DEFAULT_AREAS.get("SAAP"), Arrays.asList("Dine Utbetalinger","DittNAV", "nav.no", "Søknadsdialoger"));
+
+        AREAS_INSTANCES.put(DEFAULT_AREAS.get("SYKF"), Arrays.asList("Dokumentløsning","Gosys", "Modiabrukerdialog", "Navet","Personkortet","WinTid"));
+        AREAS_INSTANCES.put(DEFAULT_AREAS.get("TILR"), Arrays.asList("Ok"));
+        AREAS_INSTANCES.put(DEFAULT_AREAS.get("OKOH"), Arrays.asList("AAregister","AktørRegister", "Datavarehus","DKIF", "Enhetsregister","Henvendelse", "Inntektskomponenten","Joark","NORG","TPS","TSS"));
+
+        AREAS_INSTANCES.put(DEFAULT_AREAS.get("OKHJ"), Arrays.asList("Dokumentløsning","Gosys", "Modiabrukerdialog", "Navet","Personkortet","WinTid"));
+        AREAS_INSTANCES.put(DEFAULT_AREAS.get("BEOS"), Arrays.asList("Ok"));
+        AREAS_INSTANCES.put(DEFAULT_AREAS.get("MNIF"), Arrays.asList("AAregister","AktørRegister", "Datavarehus","DKIF", "Enhetsregister","Henvendelse", "Inntektskomponenten","Joark","NORG","TPS","TSS"));
 
     }
 
