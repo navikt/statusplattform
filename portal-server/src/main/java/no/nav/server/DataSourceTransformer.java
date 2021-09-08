2bb09dc8f7de5a6263200e1ce19e99ee9f863d99
larsLotNav <lars.august.lotsberg@nav.no>
2021-09-08 10:42:25 +0200

    Tester forbindelse mot db

--- portal-server/src/main/java/no/nav/server/DataSourceTransformer.java
+++ portal-server/src/main/java/no/nav/server/DataSourceTransformer.java
@@ -5,15 +5,24 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.flywaydb.core.Flyway;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
 public class DataSourceTransformer {
-    public static DataSource create(Map<String, String> props) {
+    public static DataSource create(Map<String, String> props1) {
+        Map<String, String> props = new HashMap<>();
+        props.put("dataSource.jdbcUrl", "jdbc:postgresql://localhost:5432/navstatus");
+        props.put("dataSource.username", "postgres");
+        props.put("dataSource.password", System.getenv("key1"));
+        props.put("dataSource.driverClassName", "org.postgresql.Driver");
         Properties properties = new Properties();
         props.forEach(properties::put);
         HikariDataSource dataSource = new HikariDataSource(new HikariConfig(properties));
         Flyway.configure().dataSource(dataSource).load().migrate();
         return dataSource;
     }
+
+
+
 }
