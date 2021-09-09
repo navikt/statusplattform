10719e193edefc06f922d81d743a9607cee1093b
larsLotNav <lars.august.lotsberg@nav.no>
2021-09-09 11:58:22 +0200

    Db-credentials test nr 6 med try, venter på proxy

--- portal-server/src/main/java/no/nav/server/DataSourceTransformer.java
+++ portal-server/src/main/java/no/nav/server/DataSourceTransformer.java
@@ -12,12 +12,12 @@ import java.util.Properties;
 public class DataSourceTransformer {
     public static DataSource create(Map<String, String> props1) {
         Map<String, String> props = new HashMap<>();
-        //props.put("jdbcUrl", "jdbc:postgresql://127.0.0.1:5432/navstatus");
-        //props.put("username", "postgres");
+        props.put("jdbcUrl", "jdbc:postgresql://127.0.0.1:5432/navstatus");
+        props.put("username", "postgres");
         //Lokalt passord
         //props.put("password", "system");
-        //props.put("password", System.getenv("key1"));
-        props.put("jdbcUrl", "jdbc:postgres://postgres:karamell64@127.0.0.1:5432/navstatus");
+        props.put("password", System.getenv("key1"));
+        //props.put("jdbcUrl", "jdbc:postgres://postgres:karamell64@127.0.0.1:5432/navstatus");
         props.put("driverClassName", "org.postgresql.Driver");
         props.put("maximumPoolSize","32");
         Properties properties = new Properties();
