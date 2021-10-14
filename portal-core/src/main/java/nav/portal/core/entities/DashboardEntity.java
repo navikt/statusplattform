881c4e9cb1abbe85e1a947fbaa5cc163ea764e0b
Christian-Haugland <christian.rudfoss.haugland@nav.no>
2021-10-14 09:36:34 +0200

    Bugfix for retrieve av listeelementer. Fikk ikke match mot flere enn en pga mangel på equals

--- portal-core/src/main/java/nav/portal/core/entities/DashboardEntity.java
+++ portal-core/src/main/java/nav/portal/core/entities/DashboardEntity.java
@@ -2,6 +2,7 @@ package nav.portal.core.entities;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 public class DashboardEntity {
@@ -30,6 +31,18 @@ public class DashboardEntity {
         return this;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DashboardEntity that = (DashboardEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
 
 
