3b1878959c0b90037eaa3ff352e3239198dd9a2d
larsLotNav <lars.august.lotsberg@nav.no>
2021-10-15 08:48:17 +0200

    Cors temp

--- portal-infrastructure/src/main/java/no/nav/portal/infrastructure/CORSFilter.java
+++ portal-infrastructure/src/main/java/no/nav/portal/infrastructure/CORSFilter.java
@@ -46,9 +46,9 @@ public class CORSFilter implements Filter {
         // Authorize (allow) all domains to consume the content
         //TODO hvordan skal dette håndteres? Dersom frontend og backend hostes på samme server trenger ikke
         String frontendUrl = isLocal? "localhost" : "portal.labs.nais.io";
-        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", frontendUrl);
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST, DELETE");
-        //((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers", "*");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers", "*");
 
 
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
