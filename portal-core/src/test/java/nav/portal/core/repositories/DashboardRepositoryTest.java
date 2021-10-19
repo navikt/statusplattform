c46fb76adcb7acfc429fe53fa1a50e563b0dca8d
M137316 <orlene.mitchell@nav.no>
2021-10-19 13:28:53 +0200

    Begynt på DashboardRepositoryTest

---
+++ portal-core/src/test/java/nav/portal/core/repositories/DashboardRepositoryTest.java
@@ -0,0 +1,138 @@
+package nav.portal.core.repositories;
+
+import nav.portal.core.entities.AreaEntity;
+import nav.portal.core.entities.AreaWithServices;
+import nav.portal.core.entities.DashboardEntity;
+import nav.portal.core.entities.ServiceEntity;
+import org.fluentjdbc.DbContext;
+import org.fluentjdbc.DbContextConnection;
+import org.junit.jupiter.api.AfterEach;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.sql.DataSource;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+class DashboardRepositoryTest {
+
+    private SampleData sampleData = new SampleData();
+
+    private DataSource dataSource = TestDataSource.create();
+
+    private DbContext dbContext = new DbContext();
+    private DbContextConnection connection;
+
+    @BeforeEach
+    void startConnection() {
+        connection = dbContext.startConnection(dataSource);
+    }
+
+    @AfterEach
+    void endConnection() {
+        connection.close();
+    }
+
+    private final DashboardRepository dashboardRepository = new DashboardRepository(dbContext);
+    private final AreaRepository areaRepository = new AreaRepository(dbContext);
+
+    @Test
+    void save() {
+        //Arrange
+        String dashboardName = sampleData.getRandomizedDashboardName();
+        //Act
+        UUID uuid = dashboardRepository.save(dashboardName);
+        //Assert
+        Assertions.assertThat(uuid).isNotNull();
+    }
+
+    @Test
+    void settAreasOnDashboard() {
+        //Arrange
+        //Sett opp et dashboard java-obj
+        String dashboardName = sampleData.getRandomizedDashboardName();
+        //Lagra java-obj ned i db
+        UUID dashboard_id = dashboardRepository.save(dashboardName);
+        //Sett opp en liste av områder(area)
+        List<AreaEntity> areas = sampleData.getRandomLengthListOfAreaEntity();
+        //Lagrer alle ned i db
+
+
+//         Alternativ 1 med bruk av for loop
+        List<UUID> areas_ids = new ArrayList<>();
+        for(AreaEntity area: areas){
+            UUID id = areaRepository.save(area);
+            areas_ids.add(id);
+            area.setId(id);
+        }
+//         * Alternativ 2 med bruk av for-each loop og lambda
+//        areas.forEach(area -> ids.add(areaRepository.save(area)));
+//
+//        // Alternativ 3 med bruk av stream og lambda
+//        List<UUID> areas_ids =  areas.stream()
+//                .map(areaRepository::save)
+//                .collect(Collectors.toList());
+
+        //Act
+        //Knytter områdene til dashboard
+        dashboardRepository.settAreasOnDashboard(dashboard_id,areas_ids);
+        Map.Entry<DashboardEntity,List<AreaWithServices>> dashboardWithAreas = dashboardRepository.retrieveOne(dashboard_id);
+
+        //Assert
+        //Sjekker at id på dashboard er riktig
+        Assertions.assertThat(dashboardWithAreas.getKey().getId()).isEqualTo(dashboard_id);
+
+        //Sjekker at dashboard navnet er riktig
+        Assertions.assertThat(dashboardWithAreas.getKey().getName()).isEqualTo(dashboardName);
+
+        //Sjekke at områdene er blitt lagt til riktig
+        List<AreaEntity> retrievedAreas =  dashboardWithAreas.getValue()
+                .stream()
+                .map(AreaWithServices::getArea)
+                .collect(Collectors.toList());
+        Assertions.assertThat(retrievedAreas).isEqualTo(areas);
+
+
+        //Sjekker at ingen av områdene har tjenester knyttet til seg
+       List<List<ServiceEntity>> servicesOnAreas =  dashboardWithAreas.getValue()
+                    .stream()
+                    .map(AreaWithServices::getServices)
+                    .collect(Collectors.toList());
+
+       servicesOnAreas.forEach(list -> Assertions.assertThat(list).isEmpty());
+
+    }
+
+    @Test
+    void getAllDashboardUUIDsAndNames() {
+        //Arrange
+        //Act
+        //Assert
+    }
+
+    @Test
+    void uidFromName() {
+    }
+
+    @Test
+    void retrieveOne() {
+    }
+
+    @Test
+    void retrieveOneFromName() {
+    }
+
+    @Test
+    void retrieveAll() {
+    }
+
+    @Test
+    void deleteAreasFromDashboard() {
+    }
+
+    @Test
+    void deleteDashboard() {
+    }
+}
--- portal-core/src/test/java/nav/portal/core/repositories/SampleData.java
+++ portal-core/src/test/java/nav/portal/core/repositories/SampleData.java
@@ -3,8 +3,7 @@ package nav.portal.core.repositories;
 import nav.portal.core.entities.*;
 import nav.portal.core.enums.ServiceType;
 
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 
 
 public class SampleData {
@@ -13,7 +12,7 @@ public class SampleData {
             "Skal søke AAP","Har mistet noen i nær famile","Sykdom i familien", "Trenger tilrettelegging",
             "Trenger økonomisk sosialhjelp", "Trenger økonomisk rådgivning", "Berørt av EØS-saken"};
 
-    String[] dashboardNames = {"Privatperson", "Arbeidsgiver", "Sammarbeidspartner"};
+    String[] dashboardNames = {"Privatperson", "Arbeidsgiver", "Sammarbeidspartner", "Et ganske så langt navn kommer her, går dette an da?", "ÆØÅ"};
 
     String[] serviceNames = {"Service1", "Testesrvice 2", "Æ er en tjeneste", "Øgletjeneste"};
 
@@ -26,11 +25,25 @@ public class SampleData {
     String[] icons = {"0001","0002","0003","0004","0005","0006","0007","0008","0009","00010","0011","0012"};
 
 
+public String getRandomizedDashboardName(){
+    return getRandomFromArray(dashboardNames);
+}
+
 public AreaEntity getRandomizedAreaEntity(){
     return new AreaEntity()
             .setName(getRandomFromArray(areaNames))
             .setDescription(getRandomFromArray(descriptions))
             .setIcon(getRandomFromArray(icons));
+}
+
+public List<AreaEntity> getRandomLengthListOfAreaEntity(){
+    Random random = new Random();
+    int numberOfAreas = random.nextInt(12);
+    List<AreaEntity> areas = new ArrayList<>();
+    for(int i = 0; i < numberOfAreas; i++ ){
+        areas.add(getRandomizedAreaEntity());
+    }
+    return areas;
 }
 
 public ServiceEntity getRandomizedServiceEntity() {
