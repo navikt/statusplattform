6db0e7f273c831e87040800ba2bed7a6d9c8dde3
larsLotNav <lars.august.lotsberg@nav.no>
2021-10-27 10:02:06 +0200

    Rettet feil i url for endepunkter

--- portal-rest-api/src/main/java/no/nav/portal/rest/api/v3/controllers/ServiceController.java
+++ portal-rest-api/src/main/java/no/nav/portal/rest/api/v3/controllers/ServiceController.java
@@ -43,14 +43,14 @@ public class ServiceController {
         serviceRepositoryHelper.updateService(serviceDto);
     }
 
-    @PUT("/Service/addDependency/:Service_id/:DependentOnService_id")
+    @PUT("/Service/:Service_id/:DependentOnService_id")
     @JsonBody
     public void newService(@PathParam("Service_id") UUID service_id
             ,@PathParam("DependentOnService_id") UUID dependentOnService_id) {
         serviceRepository.addDependencyToService(service_id,dependentOnService_id);
     }
 
-    @DELETE("/Service/addDependency/:Service_id/:DependentOnService_id")
+    @DELETE("/Service/:Service_id/:DependentOnService_id")
     public void removeDependencyFromService(@PathParam("Service_id") UUID service_id
             ,@PathParam("DependentOnService_id") UUID dependentOnService_id) {
         serviceRepository.removeDependencyFromService(service_id,dependentOnService_id);
