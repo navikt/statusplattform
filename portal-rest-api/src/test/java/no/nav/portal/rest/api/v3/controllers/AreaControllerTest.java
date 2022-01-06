421fa93323921c9892368c3966312849537c8030
M137316 <orlene.mitchell@nav.no>
2022-01-06 09:15:50 +0100

    Testing fortsetter

--- portal-rest-api/src/test/java/no/nav/portal/rest/api/v3/controllers/AreaControllerTest.java
+++ portal-rest-api/src/test/java/no/nav/portal/rest/api/v3/controllers/AreaControllerTest.java
@@ -1,35 +1,77 @@
 package no.nav.portal.rest.api.v3.controllers;
 
+import org.fluentjdbc.DbContext;
+import org.fluentjdbc.DbContextConnection;
+import nav.portal.core.entities.AreaEntity;
 import no.portal.web.generated.api.AreaDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-
 
 import nav.portal.core.repositories.*;
 import no.nav.portal.rest.api.EntityDtoMappers;
+
 import no.nav.portal.rest.api.Helpers.AreaRepositoryHelper;
 import no.portal.web.generated.api.*;
 import org.actioncontroller.*;
 import org.actioncontroller.json.JsonBody;
 import org.fluentjdbc.DbContext;
 
+
+import javax.sql.DataSource;
 import java.util.*;
+import java.util.stream.Collectors;
 
 
-
+import static no.nav.portal.rest.api.EntityDtoMappers.toAreaDtoShallow;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AreaControllerTest {
 
+    private final SampleData sampleData = new SampleData();
+    private final DataSource dataSource = TestDataSource.create();
+    private final DbContext dbContext = new DbContext();
+
+    private DbContextConnection connection;
+
+    private final AreaController areaController = new AreaController(dbContext);
+    private final AreaRepository areaRepository = new AreaRepository(dbContext);
+    private final ServiceController serviceController = new ServiceController(dbContext);
+    private final ServiceRepository serviceRepository = new ServiceRepository(dbContext);
+    private final RecordRepository recordRepository = new RecordRepository(dbContext);
+
+
+    @BeforeEach
+    void startConnection() {
+        connection = dbContext.startConnection(dataSource);
+    }
+
+    @AfterEach
+    void endConnection() {
+        TestUtil.clearAllTableData(dbContext);
+        connection.close();
+    }
+
     @Test
     void getAllAreas() {
+        //Arrange
+        AreaEntity area = sampleData.getRandomizedAreaEntity();
+        //Act
+
+        //Assert
+
 
     }
 
     @Test
     void newArea() {
+        //Arrange
+        AreaEntity area = sampleData.getRandomizedAreaEntity();
+        //Act
+        AreaDto areaDto = areaController.newArea(EntityDtoMappers.toAreaDtoShallow(area));
+        //Assert
+        Assertions.assertThat(areaDto).isNotNull();
     }
 
     @Test
--- portal-rest-api/src/test/java/no/nav/portal/rest/api/v3/controllers/ServiceControllerTest.java
+++ portal-rest-api/src/test/java/no/nav/portal/rest/api/v3/controllers/ServiceControllerTest.java
@@ -6,6 +6,8 @@ import nav.portal.core.repositories.*;
 import no.nav.portal.rest.api.EntityDtoMappers;
 import no.portal.web.generated.api.ServiceDto;
 
+import org.actioncontroller.PathParam;
+import org.actioncontroller.json.JsonBody;
 import org.fluentjdbc.DbContext;
 import org.fluentjdbc.DbContextConnection;
 import org.junit.jupiter.api.AfterEach;
@@ -45,42 +47,45 @@ class ServiceControllerTest {
     @Test
     void getServices() {
         //Arrange
-        int NumberOfServices = 4;
-        List<ServiceEntity> services = sampleData.getNonEmptyListOfServiceEntity(NumberOfServices);
+        List<ServiceEntity> services = sampleData.getNonEmptyListOfServiceEntity(3);
 
         //Lagrer tjenester
         services.forEach(s -> s.setId(serviceRepository.save(s)));
 
+        Map<UUID, RecordEntity> servicesWithStatus= new HashMap<>();
 
         //Lager tilfeldig status for hver tjeneste
-        Map<UUID, RecordEntity> servicesWithStatus= new HashMap<>();
         services.forEach(s -> servicesWithStatus.put(s.getId(), SampleData.getRandomizedRecordEntityForService(s)));
+        //Lagrer statusen på tjenesten
+        servicesWithStatus.keySet().forEach(id -> recordRepository.save(servicesWithStatus.get(id)));
 
-        //Lagrer statusen på tjenesten til DB
-        servicesWithStatus.values().forEach(recordRepository::save);
 
-        //En av tjenestene er avhengig av resten
-        ServiceEntity serviceWithDependensies = services.get(0);
-        List<ServiceEntity> dependencies = services.subList(1,services.size());
-        UUID expectedUUIDOfServiceWithDependecies = serviceWithDependensies.getId();
+        //TODO legg in avhengigheter her før mappingen:
+        //serviceRepository.addDependencyToService();
+        //Under bygges forventet dtoer m status og avhengigheter utifra oppsettet over:
+        List<ServiceDto> expectedDtos = services.stream()
+                .map(s->
+                        EntityDtoMappers.toServiceDtoDeep(s, Collections.emptyList()))
+                .map(dto -> setStatus(servicesWithStatus, dto))
+                .collect(Collectors.toList());
 
-        serviceRepository.addDependencyToService(serviceWithDependensies, dependencies);
+        //TODO Orlene: Legge til avhengigheter og statuser på tjenestene
+        // Først lagre avhengigheter til repository
+        ServiceEntity service = sampleData.getRandomizedServiceEntityWithNameNotInList(services);
+        service.setId(serviceRepository.save(service));
+        RecordEntity serviceRecord = sampleData.getRandomizedRecordEntityForService(service);
 
+        // Legge til avhengighetene i mappingen, se der det står Collections.emptyList() -> Liste av avhengigheter
+        serviceRepository.addDependencyToService(service, services);
+
+        //recordRepository.save()
+        UUID serviceRecId = recordRepository.save(serviceRecord);
         //Act
         List<ServiceDto> resultingDtos = serviceController.getServices();
 
         //Assert
-        Assertions.assertThat(resultingDtos.size()).isEqualTo(NumberOfServices);
+        Assertions.assertThat(resultingDtos).containsAll(expectedDtos);
 
-        //Finner alle tjenester med avhengigheter fra resultatet
-        List<ServiceDto> retrievedServicesWithDependencies = resultingDtos
-                .stream()
-                .filter(dto -> dto.getDependencies().size() > 0)
-                .collect(Collectors.toList());
-        //Forventer at det bare er en tjeneste med avhengighet
-        Assertions.assertThat(retrievedServicesWithDependencies.size()).isEqualTo(1);
-        //Forventer at den har samme UUID som entiteten vi valgte skulle ha avhengigheter
-        Assertions.assertThat(retrievedServicesWithDependencies.get(0).getId()).isEqualTo(expectedUUIDOfServiceWithDependecies);
 
     }
 
@@ -101,32 +106,88 @@ class ServiceControllerTest {
         //Act
         ServiceDto serviceDto = serviceController.getService(serviceId);
         //Assert
+        Assertions.assertThat(serviceDto).isNotNull();
+        Assertions.assertThat(serviceDto.getId()).isNotNull();
     }
 
     @Test
     void newService() {
         //Arrange
-        //Denne har ingen avhengigheter: TODO lage tilsvarende med avhengigheter
         ServiceEntity service = sampleData.getRandomizedServiceEntity();
-        ServiceDto inputDto = EntityDtoMappers.toServiceDtoShallow(service);
         //Act
-        ServiceDto actualDto  = serviceController.newService(inputDto);
+        ServiceDto serviceDto = serviceController.newService(EntityDtoMappers.toServiceDtoShallow(service));
         //Assert
-        Assertions.assertThat(actualDto.getId()).isNotNull();
-        inputDto.setId(actualDto.getId());
-        Assertions.assertThat(inputDto).isEqualTo(actualDto);
+        Assertions.assertThat(serviceDto).isNotNull();
+        Assertions.assertThat(serviceDto.getId()).isNotNull();
+
     }
 
     @Test
     void updateService() {
+        //Arrange
+        List<ServiceEntity> services = sampleData.getNonEmptyListOfServiceEntity(2);
+        ServiceDto beforeDto = serviceController.newService(EntityDtoMappers.toServiceDtoShallow(services.get(0)));
+        UUID beforeId = beforeDto.getId();
+        ServiceDto afterDto = serviceController.newService(EntityDtoMappers.toServiceDtoShallow(services.get(1)));
+        UUID afterId = afterDto.getId();
+        //Act
+        serviceController.updateService(beforeId, afterDto);
+        ServiceDto updated = serviceController.getService(beforeId);
+        //Assert
+        Assertions.assertThat(updated).isEqualTo(beforeDto);
     }
 
     @Test
     void removeDependencyFromService() {
+        //Arrange
+        List<ServiceEntity> services = sampleData.getNonEmptyListOfServiceEntity(3);
+        ServiceEntity service = sampleData.getRandomizedServiceEntityWithNameNotInList(services);
+        UUID serviceId = serviceRepository.save(service);
+        service.setId(serviceId);
+
+        for(ServiceEntity serv : services){
+            serv.setId(serviceRepository.save(serv));
+        }
+
+        UUID dependency = services.get(0).getId();
+        ServiceEntity removed = services.get(0);
+        serviceRepository.addDependencyToService(service, services);
+        Map.Entry<ServiceEntity, List<ServiceEntity>> before =
+                serviceRepository.retrieveOneWithDependencies(serviceId);
+        //Act
+        serviceController.removeDependencyFromService(serviceId,dependency);
+        Map.Entry<ServiceEntity, List<ServiceEntity>> after =
+                serviceRepository.retrieveOneWithDependencies(serviceId);
+
+        //Assert
+        Assertions.assertThat(before.getValue().size()).isEqualTo(services.size());
+        Assertions.assertThat(before.getValue()).contains(removed);
+        Assertions.assertThat(after.getValue()).doesNotContain(removed);
     }
 
     @Test
     void deleteService() {
+        //Arrange
+        List<ServiceEntity> services = sampleData.getNonEmptyListOfServiceEntity(3);
+        ServiceEntity service = sampleData.getRandomizedServiceEntityWithNameNotInList(services);
+        UUID serviceId = serviceRepository.save(service);
+        service.setId(serviceId);
+
+        for(ServiceEntity serv : services){
+            serv.setId(serviceRepository.save(serv));
+        }
+
+        serviceRepository.addDependencyToService(service, services);
+        Map.Entry<ServiceEntity, List<ServiceEntity>> before =
+                serviceRepository.retrieveOneWithDependencies(serviceId);
+        //Act
+        serviceController.deleteService(serviceId);
+        Optional<ServiceEntity> shouldBeEmpty = serviceRepository.retrieve(serviceId);
+
+        //Assert
+        Assertions.assertThat(before.getValue().size()).isEqualTo(services.size());
+        Assertions.assertThat(shouldBeEmpty).isEmpty();
+
     }
 
     @Test
