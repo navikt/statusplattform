ba1ba27e6bd8330157a8be9c0c00d7e2e0709561
larsLotNav <lars.august.lotsberg@nav.no>
2022-02-17 13:39:57 +0100

    Kan ikke ha understrek her

--- portal-rest-api/src/main/java/no/nav/portal/rest/api/v3/controllers/OPSmessegeAndMaintenanceController.java
+++ portal-rest-api/src/main/java/no/nav/portal/rest/api/v3/controllers/OPSmessegeAndMaintenanceController.java
@@ -1,10 +1,8 @@
 package no.nav.portal.rest.api.v3.controllers;
 
-import nav.portal.core.repositories.RecordRepository;
 import no.nav.portal.rest.api.Helpers.ServiceRepositoryHelper;
 import no.portal.web.generated.api.MaintenanceDto;
 import no.portal.web.generated.api.OPSmessageDto;
-import no.portal.web.generated.api.ServiceDto;
 import org.actioncontroller.POST;
 import org.actioncontroller.json.JsonBody;
 import org.fluentjdbc.DbContext;
