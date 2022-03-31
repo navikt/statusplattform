516e76bde8a448e01117752e779f795d894463a3
larsLotNav <lars.august.lotsberg@nav.no>
2022-03-31 12:42:02 +0200

    Added opening hours for service continues

--- portal-core/src/main/resources/db/migration/V010__add_opening_hours.sql
+++ portal-core/src/main/resources/db/migration/V010__add_opening_hours.sql
@@ -2,12 +2,13 @@ CREATE TABLE service_opening_hours
 (
     id UUID NOT NULL,
     service_id UUID NOT NULL,
-    day_of_the_week DOW NOT NULL,
+    day_of_the_week INT NOT NULL,
     opening_time TIME NOT NULL,
     closing_time TIME NOT NULL,
     created_at timestamp with time zone NOT NULL DEFAULT NOW(),
     updated_at timestamp with time zone NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (service_id) REFERENCES service (id)
+    FOREIGN KEY (service_id) REFERENCES service (id),
+    constraint valid_weekday check (day_of_the_week <= 6 && day_of_the_week >= 0)
 );
 
