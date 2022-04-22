aea66741978df2b81ad1f5f615871970905a93ed
larsLotNav <lars.august.lotsberg@nav.no>
2022-04-22 11:54:42 +0200

    Removed citizen user, adder ops message

---
+++ portal-core/src/main/resources/db/migration/V011__add_ops_message.sql
@@ -0,0 +1,24 @@
+CREATE TABLE ops_message
+(
+    id UUID NOT NULL,
+    intern_header VARCHAR(100) NOT NULL,
+    intern_text VARCHAR(100) NOT NULL,
+    extern_header VARCHAR(100) NOT NULL,
+    extern_text VARCHAR(100) NOT NULL,
+    isActive boolean NOT NULL,
+    onlyShowForNavEmployees boolean NOT NULL,
+    created_at timestamp with time zone NOT NULL DEFAULT NOW(),
+    updated_at timestamp with time zone NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE ops_message_service
+(
+    ops_message_id    UUID        NOT NULL,
+    service_id      UUID        NOT NULL,
+    created_at timestamp with time zone NOT NULL DEFAULT NOW(),
+    updated_at timestamp with time zone NULL,
+    PRIMARY KEY (ops_message_id, service_id),
+    FOREIGN KEY (service_id) REFERENCES service (id)
+);
+
