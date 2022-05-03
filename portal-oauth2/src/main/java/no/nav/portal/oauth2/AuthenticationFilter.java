758357702b8f770338c2397ab2fecbeb8c3b0701
larsLotNav <lars.august.lotsberg@nav.no>
2022-05-03 14:45:39 +0200

    Ser om man får hentet ut bruker i AuthenticationFilter

--- portal-oauth2/src/main/java/no/nav/portal/oauth2/AuthenticationFilter.java
+++ portal-oauth2/src/main/java/no/nav/portal/oauth2/AuthenticationFilter.java
@@ -2,8 +2,13 @@ package no.nav.portal.oauth2;
 
 
 import no.nav.portal.infrastructure.OpenIdConnectAuthentication;
+import no.nav.portal.infrastructure.PortalRestPrincipal;
+import org.eclipse.jetty.security.UserAuthentication;
 import org.eclipse.jetty.server.Authentication;
 import org.eclipse.jetty.server.Request;
+import org.jsonbuddy.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
 
@@ -11,12 +16,15 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+import java.util.Base64;
+import java.util.Optional;
 
 
 public class AuthenticationFilter implements Filter {
 
+    public static final String AUTHORIZATION_HEADER = "Authorization";
     private final Authentication authentication;
+    private static final Logger logger = LoggerFactory.getLogger(AuthenticationFilter.class);
 
     public AuthenticationFilter(Authentication authentication) {
         this.authentication = authentication;
@@ -47,9 +55,34 @@ public class AuthenticationFilter implements Filter {
             return;
         }
         if( pathInfo.startsWith("/authenticate")){
+            getUserv2(request);
             ((HttpServletRequest)request).authenticate((HttpServletResponse)response);
         }
         chain.doFilter(request, response);
+    }
+
+    private void getUserv2(ServletRequest servletRequest) {
+        logger.info("IN AUTHENTICATION FILTER, /authenticate");
+        logger.info("IN AUTHENTICATION FILTER, /authenticate");
+        logger.info("IN AUTHENTICATION FILTER, /authenticate");
+        logger.info("IN AUTHENTICATION FILTER, /authenticate");
+
+        String encodedAuthentication = ((HttpServletRequest) servletRequest).getHeader(AUTHORIZATION_HEADER);
+
+        String[] splited = encodedAuthentication.split("[.]");
+
+        String encodedHeader = splited[0];
+        String encodedPayload = splited[1];
+        String encodedSignature = splited[2];
+
+        String decodedHeader  = new String(Base64.getDecoder().decode(encodedHeader));
+        String decodedPayload  = new String(Base64.getDecoder().decode(encodedPayload));
+
+        JsonObject headerJson =  JsonObject.parse(decodedHeader);
+        JsonObject payloadJson = JsonObject.parse(decodedPayload);
+
+        logger.info("Useridentity: "+ headerJson + payloadJson);
+
     }
 
     @Override
