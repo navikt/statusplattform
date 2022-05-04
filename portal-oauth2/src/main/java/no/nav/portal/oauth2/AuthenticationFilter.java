3d19560d77bdf61847a788eb41ebaf3c76199319
larsLotNav <lars.august.lotsberg@nav.no>
2022-05-04 10:54:35 +0200

    Nytt forsøk på utlesning av claims

--- portal-oauth2/src/main/java/no/nav/portal/oauth2/AuthenticationFilter.java
+++ portal-oauth2/src/main/java/no/nav/portal/oauth2/AuthenticationFilter.java
@@ -1,8 +1,9 @@
 package no.nav.portal.oauth2;
 
 
-import no.nav.portal.infrastructure.OpenIdConnectAuthentication;
 import no.nav.portal.infrastructure.PortalRestPrincipal;
+import no.nav.security.token.support.core.jwt.JwtToken;
+import no.nav.security.token.support.core.jwt.JwtTokenClaims;
 import org.eclipse.jetty.security.DefaultUserIdentity;
 import org.eclipse.jetty.security.UserAuthentication;
 import org.eclipse.jetty.server.Authentication;
@@ -18,13 +19,9 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.attribute.UserPrincipal;
 import java.security.Principal;
 import java.util.Arrays;
 import java.util.Base64;
-import java.util.Enumeration;
-import java.util.Optional;
 
 
 public class AuthenticationFilter implements Filter {
@@ -46,7 +43,6 @@ public class AuthenticationFilter implements Filter {
 
         String pathInfo = ((Request) request).getPathInfo();
         logger.info("Path: "+ pathInfo);
-        getUserv2(request);
 
         if (pathInfo.startsWith("/login")) {
             ((HttpServletRequest)request).authenticate((HttpServletResponse)response);
@@ -54,7 +50,7 @@ public class AuthenticationFilter implements Filter {
         }
         if ( pathInfo.startsWith("/callback")) {
             logger.info("Trying to create user for request in /callback");
-            getJsonBody(request);
+            readAuthorizationFromHeader(request);
 
             ((HttpServletRequest)request).authenticate((HttpServletResponse)response);
             return;
@@ -62,22 +58,39 @@ public class AuthenticationFilter implements Filter {
         //Legg til principal her? for alle some ikke er login eller callback
         // se linje 120 i OpenIdConnectAuthentication
         logger.info("Trying to create user for request");
-        JsonObject jsonObject = getJsonBody(request);
-        if(jsonObject == null){
+        JwtTokenClaims jwtTokenClaims = readAuthorizationFromHeader(request);
+        if(jwtTokenClaims == null){
             chain.doFilter(request,response);
             return;
         }
-        PortalRestPrincipal principal = createPrincipalv2(jsonObject);
+        PortalRestPrincipal principal = createPortalPrinciplaFromAdClaims(jwtTokenClaims);
 
         Authentication authenticationForUser = new UserAuthentication("user", createUserIdentity(principal));
         ((Request) request).setAuthentication(authenticationForUser);
         chain.doFilter(request, response);
     }
 
-    public PortalRestPrincipal createPrincipalv2(JsonObject payloadJson){
+    private JwtTokenClaims readAuthorizationFromHeader(ServletRequest request) {
+        String encodedAuthentication = ((HttpServletRequest) request).getHeader(AUTHORIZATION_HEADER);
+        if (encodedAuthentication == null || encodedAuthentication.isEmpty()) {
+            return null;
+        }
+        try {
+            //TODO linja under fjerner "Bearer " fra starten av encodedAuth, denne bør kunne leses smudere
+            encodedAuthentication = encodedAuthentication.substring(7,encodedAuthentication.length()-1);
+            JwtToken jwtToken = new JwtToken(encodedAuthentication);
+            return jwtToken.getJwtTokenClaims();
+
+         }   catch (Exception e){
+            logger.info("Couldt not read encoded authentication: "+ encodedAuthentication);
+            logger.info(e.getMessage());
+        }
+        return null;
+    }
+    public PortalRestPrincipal createPortalPrinciplaFromAdClaims(JwtTokenClaims jwtTokenClaims){
         System.out.println("createPrincipal ---------------------------");
         //payloadJson =
-        return new PortalRestPrincipal(payloadJson.requiredString("name"), payloadJson.stringValue("NAVident").orElse(null));
+        return new PortalRestPrincipal(jwtTokenClaims.getStringClaim("name"), jwtTokenClaims.getStringClaim("NAVident"));
     }
 
     private DefaultUserIdentity createUserIdentity(Principal principal) {
@@ -86,64 +99,7 @@ public class AuthenticationFilter implements Filter {
         subject.getPrincipals().add(principal);
         return new DefaultUserIdentity(subject, principal, new String[0]);
     }
-    private void getUserv2(ServletRequest servletRequest) {
-        logger.info("IN AUTHENTICATION FILTER, /authenticate");
-        try {
 
-            String encodedAuthentication = ((HttpServletRequest) servletRequest).getHeader(AUTHORIZATION_HEADER);
-
-            if (encodedAuthentication == null || encodedAuthentication.isEmpty()) {
-                return;
-            }
-            logger.info("Encoded: "+ encodedAuthentication);
-            String[] splited = encodedAuthentication.split("[.]");
-
-            String encodedHeader = splited[0];
-            String encodedPayload = splited[1];
-            //String encodedSignature = splited[2];
-
-            String decodedHeader = new String(Base64.getDecoder().decode(encodedHeader));
-            String decodedPayload = new String(Base64.getDecoder().decode(encodedPayload));
-
-            JsonObject headerJson = JsonObject.parse(decodedHeader);
-            JsonObject payloadJson = JsonObject.parse(decodedPayload);
-
-            logger.info("Useridentity: " + headerJson + payloadJson);
-        }
-        catch (Exception e){
-            logger.info("Could not read authorization token");
-            logger.info(e.getMessage());
-            Arrays.stream(e.getStackTrace()).forEach(stackTraceElement -> logger.info(stackTraceElement.toString()));
-        }
-
-    }
-
-    private JsonObject getJsonBody(ServletRequest request) {
-        String encodedAuthentication = ((HttpServletRequest) request).getHeader(AUTHORIZATION_HEADER);
-        if (encodedAuthentication == null || encodedAuthentication.isEmpty()) {
-            return null;
-        }
-        try {
-            logger.info("In get jsonBody, encodedAuthentication: "+ encodedAuthentication);
-
-            String[] splited = encodedAuthentication.split("[.]");
-            String encodedHeader = splited[0];
-            String encodedPayload = splited[1];
-            String encodedSignature = splited[2];
-            //byte[] decodedBytes = Base64.getDecoder().decode(encodedAuthentication);
-            String decodedHeader = new String(Base64.getDecoder().decode(encodedHeader));
-            String decodedPayload = new String(Base64.getDecoder().decode(encodedPayload));
-            logger.info("decodedHeader: "+ encodedAuthentication);
-            logger.info("decodedPayload: "+ encodedAuthentication);
-            JsonObject headerJson = JsonObject.parse(decodedHeader);
-            JsonObject payloadJson = JsonObject.parse(decodedPayload);
-            return payloadJson;
-         }   catch (Exception e){
-            logger.info("Couldt not read encoded authentication: "+ encodedAuthentication);
-            logger.info(e.getMessage());
-        }
-        return null;
-    }
 
     @Override
     public void init(FilterConfig filterConfig) {
