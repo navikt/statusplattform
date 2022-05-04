526e12123a6dd5af9017cf6cea46e0109a05a06c
larsLotNav <lars.august.lotsberg@nav.no>
2022-05-04 10:05:54 +0200

    Uten hardkoded json

--- portal-oauth2/src/main/java/no/nav/portal/oauth2/AuthenticationFilter.java
+++ portal-oauth2/src/main/java/no/nav/portal/oauth2/AuthenticationFilter.java
@@ -56,7 +56,11 @@ public class AuthenticationFilter implements Filter {
         // se linje 120 i OpenIdConnectAuthentication
         logger.info("Trying to create user for request");
         JsonObject jsonObject = getJsonBody(request);
+        if(jsonObject == null){
+            chain.doFilter(request,response);
+        }
         PortalRestPrincipal principal = createPrincipalv2(jsonObject);
+
         Authentication authenticationForUser = new UserAuthentication("user", createUserIdentity(principal));
         ((Request) request).setAuthentication(authenticationForUser);
         chain.doFilter(request, response);
@@ -107,18 +111,28 @@ public class AuthenticationFilter implements Filter {
     }
 
     private JsonObject getJsonBody(ServletRequest request) {
-        String encodedAuthentication = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6ImpTMVhvMU9XRGpfNTJ2YndHTmd2UU8yVnpNYyJ9.eyJhdWQiOiI5ZWUwODAxOC1jNjA4LTRjNjAtYWEzYy00MDNmNmZjNmU5MTQiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vNjIzNjY1MzQtMWVjMy00OTYyLTg4NjktOWI1NTM1Mjc5ZDBiL3YyLjAiLCJpYXQiOjE2NTE0ODAwMjQsIm5iZiI6MTY1MTQ4MDAyNCwiZXhwIjoxNjUxNDg0NTgwLCJhaW8iOiJBVlFBcS84VEFBQUFYdmxVdGVLaTZpeWZXczl2c1ZtbVJXY0NDV2NpdCtld1dyK2dNQkQ2cW5iZnMvRmVJWGhFYytPZDJmSTIvNmhPOWpyNWFhNTF1bjJFYWgrVDNld0R4V0xmS282WnR5UTlvVm8xQ0pZVWJyUT0iLCJhenAiOiI5ZWUwODAxOC1jNjA4LTRjNjAtYWEzYy00MDNmNmZjNmU5MTQiLCJhenBhY3IiOiIyIiwiZ3JvdXBzIjpbIjJkN2YxYzBkLTU3ODQtNGY4MS04YmIyLThmM2E3OWY4Zjk0OSJdLCJuYW1lIjoiTG90c2JlcmcsIExhcnMgQXVndXN0Iiwib2lkIjoiMTAzZjlhZDItMGY2Yi00ODljLWFkNDQtYjNjZWJlMjUxMWRiIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiTGFycy5BdWd1c3QuTG90c2JlcmdAbmF2Lm5vIiwicmgiOiIwLkFTQUFOR1UyWXNNZVlrbUlhWnRWTlNlZEN4aUE0SjRJeG1CTXFqeEFQMl9HNlJRZ0FEay4iLCJzY3AiOiJkZWZhdWx0YWNjZXNzIiwic3ViIjoiRldHc0xGUGxsdWwyZkMzQVNNZGdiZXlPZDhreE9XM0Y5S2lIdGZFa19oUSIsInRpZCI6IjYyMzY2NTM0LTFlYzMtNDk2Mi04ODY5LTliNTUzNTI3OWQwYiIsInV0aSI6ImJReHJJbWJubGt5bG9PS0c5d0lyQUEiLCJ2ZXIiOiIyLjAiLCJOQVZpZGVudCI6IkwxNTI0MjMiLCJhenBfbmFtZSI6ImRldi1nY3A6bmF2ZGlnOnBvcnRhbHNlcnZlciJ9.r9c2p0ukf2xcXPTHkHmsFtG3ScbCRnHxMMYr-STGvmqjbzYNkthBBjmj6bC7iN6syfJ-jK8CzfMOq9wt2Hv7HwfCFu5KcnAIfVJbsaUU9qRz2DYr7OWbjYpbKZ2qg_Ko9IcI3ct931UpOfb5Z2kRQbwJHg6NRGw-Iq7xLGEXe3vKXnc-fTeFVtbRdGuxv8TTfGhHyLP9UVEVPfz_C_LH62iAgQLdDRGA4BU-mOwTRO9Zgndg74ojOIne9SXRQBpxrpNBtTpGOOsefE4GXK1T9zvGo2Rdh5Zh9fuUoQ7h0IV6BwndxiKBegPCVvrqQqIbdzJI7nXcJQJ2PsTXrxaQgQ";
-        String[] splited = encodedAuthentication.split("[.]");
-        String encodedHeader = splited[0];
-        String encodedPayload = splited[1];
-        String encodedSignature = splited[2];
-        //byte[] decodedBytes = Base64.getDecoder().decode(encodedAuthentication);
-        String decodedHeader  = new String(Base64.getDecoder().decode(encodedHeader));
-        String decodedPayload  = new String(Base64.getDecoder().decode(encodedPayload));
-        JsonObject headerJson =  JsonObject.parse(decodedHeader);
-        JsonObject payloadJson = JsonObject.parse(decodedPayload);
-        String hallo = "hallo";
-        return payloadJson;
+        String encodedAuthentication = ((HttpServletRequest) request).getHeader(AUTHORIZATION_HEADER);
+        if (encodedAuthentication == null || encodedAuthentication.isEmpty()) {
+            return null;
+        }
+        try {
+
+
+            String[] splited = encodedAuthentication.split("[.]");
+            String encodedHeader = splited[0];
+            String encodedPayload = splited[1];
+            String encodedSignature = splited[2];
+            //byte[] decodedBytes = Base64.getDecoder().decode(encodedAuthentication);
+            String decodedHeader = new String(Base64.getDecoder().decode(encodedHeader));
+            String decodedPayload = new String(Base64.getDecoder().decode(encodedPayload));
+            JsonObject headerJson = JsonObject.parse(decodedHeader);
+            JsonObject payloadJson = JsonObject.parse(decodedPayload);
+            return payloadJson;
+         }   catch (Exception e){
+            logger.info("Couldt not read encoded authentication: "+ encodedAuthentication);
+            logger.info(e.getMessage());
+        }
+        return null;
     }
 
     @Override
