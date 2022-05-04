1480a01bf77c7c801cc4b13a5c5f7163f5f7527d
larsLotNav <lars.august.lotsberg@nav.no>
2022-05-04 12:44:17 +0200

    Test av JwtTokenValidationHandler

--- portal-oauth2/src/main/java/no/nav/portal/oauth2/AuthenticationFilter.java
+++ portal-oauth2/src/main/java/no/nav/portal/oauth2/AuthenticationFilter.java
@@ -2,6 +2,10 @@ package no.nav.portal.oauth2;
 
 
 import no.nav.portal.infrastructure.PortalRestPrincipal;
+import no.nav.security.token.support.core.configuration.IssuerProperties;
+import no.nav.security.token.support.core.configuration.MultiIssuerConfiguration;
+import no.nav.security.token.support.core.context.TokenValidationContext;
+import no.nav.security.token.support.core.http.HttpRequest;
 import no.nav.security.token.support.core.jwt.JwtToken;
 import no.nav.security.token.support.core.jwt.JwtTokenClaims;
 import org.eclipse.jetty.security.DefaultUserIdentity;
@@ -12,26 +16,60 @@ import org.jsonbuddy.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
-
+import no.nav.security.token.support.core.context.TokenValidationContextHolder;
+import no.nav.security.token.support.core.validation.JwtTokenValidationHandler;
 
 import javax.security.auth.Subject;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.security.Principal;
 import java.util.Arrays;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class AuthenticationFilter implements Filter {
-
+    private static String CLIENT_ID = System.getenv("AZURE_APP_CLIENT_ID");
+    private static String CLIENT_SECRET = System.getenv("AZURE_APP_CLIENT_SECRET");
+    private static String PUBLIC_JWKS = System.getenv("AZURE_OPENID_CONFIG_JWKS_URI");
+    private static String AZURE_OPENID_CONFIG_ISSUER = System.getenv("AZURE_OPENID_CONFIG_ISSUER");
+    private static URL AZURE_WELL_KNOW_URL;
+    private static String FRONTEND_LOCATION;
     public static final String AUTHORIZATION_HEADER = "Authorization";
     private final Authentication authentication;
     private static final Logger logger = LoggerFactory.getLogger(AuthenticationFilter.class);
 
+    private  JwtTokenValidationHandler jwtTokenValidationHandler;
+
+
+
+    static {
+        try{
+            AZURE_WELL_KNOW_URL = new URL(System.getenv("AZURE_APP_WELL_KNOWN_URL"));
+            FRONTEND_LOCATION = System.getenv("FRONTEND_LOCATION");
+
+        }
+        catch (MalformedURLException e){
+            logger.info(e.toString());
+
+        }
+    }
+
+
+
+
     public AuthenticationFilter(Authentication authentication) {
         this.authentication = authentication;
+        IssuerProperties issuerProperties = new IssuerProperties(AZURE_WELL_KNOW_URL);
+        Map<String, IssuerProperties> issuerPropertiesMap = new HashMap<>();
+        issuerPropertiesMap.put("AzureAd", issuerProperties);
+        MultiIssuerConfiguration multiIssuerConfiguration = new MultiIssuerConfiguration(issuerPropertiesMap);
+        this.jwtTokenValidationHandler = new JwtTokenValidationHandler(multiIssuerConfiguration);
     }
 
     @Override
@@ -51,7 +89,7 @@ public class AuthenticationFilter implements Filter {
         if ( pathInfo.startsWith("/callback")) {
             logger.info("Trying to create user for request in /callback");
             readAuthorizationFromHeader(request);
-
+            doTokenValidation((HttpServletRequest) request,(HttpServletResponse) response, chain);
             ((HttpServletRequest)request).authenticate((HttpServletResponse)response);
             return;
         }
@@ -90,6 +128,53 @@ public class AuthenticationFilter implements Filter {
         }
         return null;
     }
+    private JwtToken getJwtToken(ServletRequest request) {
+        String encodedAuthentication = ((HttpServletRequest) request).getHeader(AUTHORIZATION_HEADER);
+        logger.info("In readAuthorizationFromHeader : ");
+        logger.info("encodedAuthentication: " + encodedAuthentication);
+
+        if (encodedAuthentication == null || encodedAuthentication.isEmpty()) {
+            return null;
+        }
+        try {
+            //TODO linja under fjerner "Bearer " fra starten av encodedAuth, denne bør kunne leses smudere
+            encodedAuthentication = encodedAuthentication.substring(7,encodedAuthentication.length()-1);
+            return new JwtToken(encodedAuthentication);
+
+        }   catch (Exception e){
+            logger.info("Couldt not read encoded authentication: "+ encodedAuthentication);
+            logger.info(e.getMessage());
+        }
+        return null;
+    }
+
+
+    private void doTokenValidation(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        logger.info("In doTokenValidation");
+        Map<String, JwtToken> issuerShortNameValidatedTokenMap = new HashMap<>();
+        issuerShortNameValidatedTokenMap.put("AzureAd", getJwtToken(request));
+        TokenValidationContextHolder contextHolder = new TokenValidationContextHolder() {
+            @Override
+            public TokenValidationContext getTokenValidationContext() {
+                return null;
+            }
+
+            @Override
+            public void setTokenValidationContext(TokenValidationContext tokenValidationContext) {
+
+            }
+        };
+        contextHolder.setTokenValidationContext(this.jwtTokenValidationHandler.getValidatedTokens(fromHttpServletRequest(request)));
+
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            contextHolder.setTokenValidationContext((TokenValidationContext)null);
+        }
+
+    }
+
+
     public PortalRestPrincipal createPortalPrinciplaFromAdClaims(JwtTokenClaims jwtTokenClaims){
         System.out.println("createPrincipal ---------------------------");
         //payloadJson =
@@ -104,6 +189,30 @@ public class AuthenticationFilter implements Filter {
     }
 
 
+
+    static HttpRequest fromHttpServletRequest(final HttpServletRequest request) {
+        return new HttpRequest() {
+            public String getHeader(String headerName) {
+                return request.getHeader(headerName);
+            }
+
+            public NameValue[] getCookies() {
+                return request.getCookies() == null ? null : (NameValue[])Arrays.stream(request.getCookies()).map((cookie) -> {
+                    return new NameValue() {
+                        public String getName() {
+                            return cookie.getName();
+                        }
+
+                        public String getValue() {
+                            return cookie.getValue();
+                        }
+                    };
+                }).toArray((x$0) -> {
+                    return new NameValue[x$0];
+                });
+            }
+        };
+    }
     @Override
     public void init(FilterConfig filterConfig) {
 
