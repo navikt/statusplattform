c679e7b1935fe2e709a26813a76872b7de001af8
larsLotNav <lars.august.lotsberg@nav.no>
2022-06-14 14:49:00 +0200

    Adding all columns

--- portal-core/src/main/resources/db/migration/V013__add_additional_fields_to_ops_message.sql
+++ portal-core/src/main/resources/db/migration/V013__add_additional_fields_to_ops_message.sql
@@ -1,5 +1,4 @@
-ALTER TABLE ops_message
-     ADD COLUMN start_time timestamp with time zone NULL,
-     ADD COLUMN end_time Date,
-     ADD COLUMN severity VARCHAR(10),
-     ADD COLUMN state VARCHAR(10);
+ALTER TABLE ops_message ADD COLUMN start_time timestamp with time zone NULL;
+ALTER TABLE ops_message ADD COLUMN end_time Date;
+ALTER TABLE ops_message ADD COLUMN severity VARCHAR(10);
+ALTER TABLE ops_message ADD COLUMN state VARCHAR(10);
