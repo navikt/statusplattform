34231603d7d6d4e55540c4e2d25e525bef17b880
larsLotNav <lars.august.lotsberg@nav.no>
2022-06-17 11:10:13 +0200

    Endepunkter for statusholder

--- portal-rest-api/src/main/java/no/nav/portal/rest/api/v3/controllers/ServiceController.java
+++ portal-rest-api/src/main/java/no/nav/portal/rest/api/v3/controllers/ServiceController.java
@@ -1,6 +1,9 @@
 package no.nav.portal.rest.api.v3.controllers;
 
 
+import nav.portal.core.entities.RecordEntity;
+import nav.portal.core.entities.ServiceEntity;
+import nav.portal.core.enums.ServiceStatus;
 import nav.portal.core.repositories.ServiceRepository;
 import no.nav.portal.rest.api.EntityDtoMappers;
 import no.nav.portal.rest.api.Helpers.ServiceControllerHelper;
@@ -8,7 +11,18 @@ import no.portal.web.generated.api.*;
 import org.actioncontroller.*;
 import org.actioncontroller.json.JsonBody;
 import org.fluentjdbc.DbContext;
+import org.jsonbuddy.JsonObject;
 
+
+import javax.json.Json;
+import javax.json.JsonReader;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -18,6 +32,7 @@ public class ServiceController {
 
     private final ServiceControllerHelper serviceControllerHelper;
     private final ServiceRepository serviceRepository;
+    private String STATUSHOLDER_URL = System.getenv("statusholder_url");
 
 
     public ServiceController(DbContext dbContext) {
@@ -123,5 +138,124 @@ public class ServiceController {
     public List<String> getServiceStatuses() {
         return StatusDto.getValues();
     }
+
+
+    @GET("/Services/Statusholder")
+    @JsonBody
+    public JsonObject getStatusHolderStatuses() throws IOException  {
+        try{
+            return getAllStatusesFromStatusholder();
+        }
+        catch (IOException e){
+            return toJson("'error':'couldNotReadFromStatusholder'");
+        }
+    }
+
+    @POST("/Service/Statusholder/:Service_id/:Status")
+    @JsonBody
+    public int addStatusToStatusholder(@PathParam("Service_id") UUID service_id
+            ,@PathParam("Status") String status) {
+        try{
+            return putToStatusholder(service_id,status);
+        }
+        catch (IOException e){
+            return 666;
+        }
+
+    }
+
+
+
+
+
+
+    private Integer calcResponseTime(LocalDateTime before, LocalDateTime after) {
+        Duration duration = Duration.between(after, before);
+        return duration.toMillisPart();
+    }
+
+
+
+
+
+
+
+    private ZonedDateTime readDateTimeFromString(String str){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss.SSS zzz");
+        return ZonedDateTime.from(formatter.parse(str));
+
+    }
+
+    private static JsonObject toJson(String str){
+        JsonReader jsonReader = Json.createReader(new StringReader(str));
+        JsonObject object = (JsonObject) jsonReader.readObject();
+        jsonReader.close();
+        return object;
+    }
+
+    private static String readBody(HttpURLConnection con) throws IOException {
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        return content.toString();
+    }
+
+
+    private JsonObject getAllStatusesFromStatusholder() throws IOException  {
+        HttpURLConnection con = getAllStatusesFromStatusholderConnection();
+        String stringBody = readBody(con);
+        JsonObject body = toJson(stringBody);
+        return body;
+    }
+
+
+
+
+    private HttpURLConnection getAllStatusesFromStatusholderConnection() throws IOException {
+
+        String STATUSHOLDER_URL = "https://statusholder.dev-fss-pub.nais.io/status";
+
+        URL url = new URL(STATUSHOLDER_URL);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        return con;
+    }
+
+
+
+
+
+    private int putToStatusholder(UUID id, String status) throws IOException {
+        HttpURLConnection con = putToStatusholderConnection(id, status);
+        return con.getResponseCode();
+
+    }
+
+
+
+
+    private HttpURLConnection putToStatusholderConnection(UUID id, String status) throws IOException {
+        //Logikken under må bort på et tidspunkt. Dette er for polling av mock data.
+
+
+
+        URL url = new URL(STATUSHOLDER_URL);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Accept", "application/json");
+        con.setRequestMethod("POST");
+        String jsonInputString = "{'serviceId':"+id+", 'status': "+status+"}";
+        try(OutputStream os = con.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+        return con;
+    }
+
 
 }
