c87568645269e959f82930838bbbd0aa3cc16fff
larsLotNav <lars.august.lotsberg@nav.no>
2022-06-27 10:49:19 +0200

    Small change in formatting of sql

--- portal-core/src/main/resources/db/migration/V015__alter_ops_message_varchar_length.sql
+++ portal-core/src/main/resources/db/migration/V015__alter_ops_message_varchar_length.sql
@@ -1,4 +1,3 @@
-ALTER TABLE ops_message
-ALTER COLUMN intern_text type VARCHAR(500),
-ALTER COLUMN extern_text type VARCHAR(500),
-DROP COLUMN state;
+ALTER TABLE ops_message ALTER COLUMN intern_text type VARCHAR(500);
+ALTER TABLE ops_message ALTER COLUMN extern_text type VARCHAR(500);
+ALTER TABLE ops_message DROP COLUMN state;
