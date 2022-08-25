6ef44fe861382978b3cdd85c7e8e72283330c1d1
larsLotNav <lars.august.lotsberg@nav.no>
2022-08-25 13:54:07 +0200

    Removed logging in auth filter

--- portal-infrastructure/src/main/java/no/nav/portal/infrastructure/AuthenticationFilter.java
+++ portal-infrastructure/src/main/java/no/nav/portal/infrastructure/AuthenticationFilter.java
@@ -132,8 +132,8 @@ public class AuthenticationFilter implements Filter {
 
     private JwtTokenClaims readAuthorizationFromHeader(ServletRequest request) {
         String encodedAuthorization = ((HttpServletRequest) request).getHeader(AUTHORIZATION_HEADER);
-        logger.info("In readAuthorizationFromHeader : ");
-        logger.info("encodedAuthorization: " + encodedAuthorization);
+        //logger.info("In readAuthorizationFromHeader : ");
+       // logger.info("encodedAuthorization: " + encodedAuthorization);
         Enumeration<String> headersNames =  ((HttpServletRequest) request).getHeaderNames();
         //logger.info("All headers: ");
         while (headersNames.hasMoreElements()){
@@ -147,7 +147,7 @@ public class AuthenticationFilter implements Filter {
             //TODO linja under fjerner "Bearer " fra starten av encodedAuth, denne bør kunne leses smudere. Bruk JwtTokenValidationHandler.
             encodedAuthorization = encodedAuthorization.substring(7,encodedAuthorization.length()-1);
             JwtToken jwtToken = new JwtToken(encodedAuthorization);
-            logger.info("jwtToken: "+ jwtToken.getJwtTokenClaims());
+            //logger.info("jwtToken: "+ jwtToken.getJwtTokenClaims());
             return jwtToken.getJwtTokenClaims();
 
         }   catch (Exception e){
