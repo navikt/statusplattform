2db80b44eb03f74cb49085e24b9332475267c9f5
larsLotNav <lars.august.lotsberg@nav.no>
2022-11-25 09:51:09 +0100

    Oauth2

---
+++ portal-infrastructure/src/main/java/no/nav/portal/infrastructure/Oauth2TokenValidator.java
@@ -0,0 +1,102 @@
+package no.nav.portal.infrastructure;
+
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.jwk.source.*;
+import com.nimbusds.jose.proc.*;
+import com.nimbusds.jwt.*;
+import com.nimbusds.jwt.proc.*;
+import no.nav.security.token.support.core.jwt.JwtToken;
+import no.nav.security.token.support.core.jwt.JwtTokenClaims;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.HashSet;
+
+public class Oauth2TokenValidator {
+    private static final Logger logger = LoggerFactory.getLogger(Oauth2TokenValidator.class);
+    String remoteJWKUrl = System.getenv("AZURE_OPENID_CONFIG_JWKS_URI");
+    String configIssuer = System.getenv("AZURE_OPENID_CONFIG_ISSUER");
+    public static final String AUTHORIZATION_HEADER = "authorization";
+
+    public void validateToken() throws BadJOSEException, ParseException, JOSEException, MalformedURLException {
+        // The access token to validate, typically submitted with a HTTP header like
+        // Authorization: Bearer eyJraWQiOiJDWHVwIiwidHlwIjoiYXQrand0IiwiYWxnIjoi...
+        String accessToken =
+                "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSJ9.eyJhdWQiOiI2YTdlNjhiMC04ODZiLTRiZDItOGU5YS0yYTU4ZTU5YzE2MzIiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vOTY2YWM1NzItZjViNy00YmJlLWFhODgtYzc2NDE5YzBmODUxL3YyLjAiLCJpYXQiOjE2NjkyODcwNzksIm5iZiI6MTY2OTI4NzA3OSwiZXhwIjoxNjY5MjkwOTc5LCJhaW8iOiJFMlpnWU5CZXIyTi83T0E5ejhCNUQxb2x6cjZiQ3dBPSIsImF6cCI6ImQ4Zjg2ZWY5LTE3MWQtNGRiZC04MjA5LWRhODg4YWU0NTcyNCIsImF6cGFjciI6IjEiLCJvaWQiOiIwMzU4YTMyNC0zYzM3LTQyMTgtOTgzNy1kYmQzMDc0MzIyOWIiLCJyaCI6IjAuQVVjQWNzVnFscmYxdmt1cWlNZGtHY0Q0VWJCb2ZtcHJpTkpManBvcVdPV2NGakpIQUFBLiIsInJvbGVzIjpbImFjY2Vzc19hc19hcHBsaWNhdGlvbiJdLCJzdWIiOiIwMzU4YTMyNC0zYzM3LTQyMTgtOTgzNy1kYmQzMDc0MzIyOWIiLCJ0aWQiOiI5NjZhYzU3Mi1mNWI3LTRiYmUtYWE4OC1jNzY0MTljMGY4NTEiLCJ1dGkiOiJtWGlGXzQxQnNFZTI5bjE0SS1YTEFBIiwidmVyIjoiMi4wIn0.o3VteE2f0vGpEsUDZRjYEADFRJ6ugXBo9BQ934Bgc4ct4msecYXNK9HEFm7CAZeRdP5rN0HB6GE3PRV3Msd6CetayvMJFx0_IeDZOKUcPK9klzTAzXp3Yq482ojCtie52BZmK7cb0uVgg2vzE29BMRVmbZrr3eahGZ-OHh2kU0jV6v5LPgkbMYTXic7ZxjaYr4vr40hCiUtpt9Ym3VUpfULPwbaZJfXxVoz3PaOb5h1XAAsd8VJtzyL1nWanLq9XlaOlBQrqXLkmulpZoOmiexY9pKhyUGKkhs-GDEwM3LqXkW6BQYUD_hMiEgfI3enmdRUXB40DRL-AM3ofhKjMNg";
+
+
+        // Create a JWT processor for the access tokens
+        ConfigurableJWTProcessor<SecurityContext> jwtProcessor =
+                new DefaultJWTProcessor<>();
+
+        // Set the required "typ" header "at+jwt" for access tokens issued by the
+        // Connect2id server, may not be set by other servers
+        jwtProcessor.setJWSTypeVerifier(
+                new DefaultJOSEObjectTypeVerifier<>(new JOSEObjectType("JWT")));
+
+        // The public RSA keys to validate the signatures will be sourced from the
+        // OAuth 2.0 server's JWK set, published at a well-known URL. The RemoteJWKSet
+        // object caches the retrieved keys to speed up subsequent look-ups and can
+        // also handle key-rollover
+        JWKSource<SecurityContext> keySource =
+                new RemoteJWKSet<>(new URL(remoteJWKUrl));
+
+        // The expected JWS algorithm of the access tokens (agreed out-of-band)
+        JWSAlgorithm expectedJWSAlg = JWSAlgorithm.RS256;
+
+        // Configure the JWT processor with a key selector to feed matching public
+        // RSA keys sourced from the JWK set URL
+        JWSKeySelector<SecurityContext> keySelector =
+                new JWSVerificationKeySelector<>(expectedJWSAlg, keySource);
+
+        jwtProcessor.setJWSKeySelector(keySelector);
+
+        // Set the required JWT claims for access tokens issued by the Connect2id
+        // server, may differ with other servers
+        jwtProcessor.setJWTClaimsSetVerifier(new DefaultJWTClaimsVerifier(
+                new JWTClaimsSet.Builder().issuer(configIssuer).build(),
+                new HashSet<>(Arrays.asList("sub", "iat", "exp"))));
+
+        // Process the token
+        SecurityContext ctx = null; // optional context parameter, not required here
+        JWTClaimsSet claimsSet = jwtProcessor.process(accessToken, ctx);
+
+        // Print out the token claims set
+       // System.out.println(claimsSet.toJSONObject());
+    }
+
+    public JwtTokenClaims readAuthorizationFromHeader(ServletRequest request){
+        String BEARER ="Bearer ";
+
+        String accessToken =
+                "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSJ9.eyJhdWQiOiI2YTdlNjhiMC04ODZiLTRiZDItOGU5YS0yYTU4ZTU5YzE2MzIiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vOTY2YWM1NzItZjViNy00YmJlLWFhODgtYzc2NDE5YzBmODUxL3YyLjAiLCJpYXQiOjE2NjkyODcwNzksIm5iZiI6MTY2OTI4NzA3OSwiZXhwIjoxNjY5MjkwOTc5LCJhaW8iOiJFMlpnWU5CZXIyTi83T0E5ejhCNUQxb2x6cjZiQ3dBPSIsImF6cCI6ImQ4Zjg2ZWY5LTE3MWQtNGRiZC04MjA5LWRhODg4YWU0NTcyNCIsImF6cGFjciI6IjEiLCJvaWQiOiIwMzU4YTMyNC0zYzM3LTQyMTgtOTgzNy1kYmQzMDc0MzIyOWIiLCJyaCI6IjAuQVVjQWNzVnFscmYxdmt1cWlNZGtHY0Q0VWJCb2ZtcHJpTkpManBvcVdPV2NGakpIQUFBLiIsInJvbGVzIjpbImFjY2Vzc19hc19hcHBsaWNhdGlvbiJdLCJzdWIiOiIwMzU4YTMyNC0zYzM3LTQyMTgtOTgzNy1kYmQzMDc0MzIyOWIiLCJ0aWQiOiI5NjZhYzU3Mi1mNWI3LTRiYmUtYWE4OC1jNzY0MTljMGY4NTEiLCJ1dGkiOiJtWGlGXzQxQnNFZTI5bjE0SS1YTEFBIiwidmVyIjoiMi4wIn0.o3VteE2f0vGpEsUDZRjYEADFRJ6ugXBo9BQ934Bgc4ct4msecYXNK9HEFm7CAZeRdP5rN0HB6GE3PRV3Msd6CetayvMJFx0_IeDZOKUcPK9klzTAzXp3Yq482ojCtie52BZmK7cb0uVgg2vzE29BMRVmbZrr3eahGZ-OHh2kU0jV6v5LPgkbMYTXic7ZxjaYr4vr40hCiUtpt9Ym3VUpfULPwbaZJfXxVoz3PaOb5h1XAAsd8VJtzyL1nWanLq9XlaOlBQrqXLkmulpZoOmiexY9pKhyUGKkhs-GDEwM3LqXkW6BQYUD_hMiEgfI3enmdRUXB40DRL-AM3ofhKjMNg";
+        //String encodedAuthorization = ((HttpServletRequest) request).getHeader(AUTHORIZATION_HEADER);
+
+        if (accessToken == null || accessToken.isEmpty() ||!accessToken.startsWith("Bearer ")) {
+            return null;
+        }
+        try {
+            accessToken = accessToken.substring(BEARER.length());
+            JwtToken jwtToken = new JwtToken(accessToken);
+            //logger.info("jwtToken: "+ jwtToken.getJwtTokenClaims());
+            return jwtToken.getJwtTokenClaims();
+
+        }   catch (Exception e){
+            logger.info("Couldt not read encoded authentication: "+ accessToken);
+            logger.info(e.getMessage());
+        }
+        return null;
+    }
+}
+
+
+
+
+
---
+++ portal-rest-api/src/main/java/no/nav/portal/rest/api/Helpers/AccessTokenHelper.java
@@ -0,0 +1,67 @@
+package no.nav.portal.rest.api.Helpers;
+
+
+
+import com.nimbusds.oauth2.sdk.*;
+import com.nimbusds.oauth2.sdk.auth.ClientAuthentication;
+import com.nimbusds.oauth2.sdk.auth.ClientSecretBasic;
+import com.nimbusds.oauth2.sdk.auth.Secret;
+import com.nimbusds.oauth2.sdk.id.ClientID;
+import com.nimbusds.oauth2.sdk.token.AccessToken;
+import com.nimbusds.oauth2.sdk.token.RefreshToken;
+
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class AccessTokenHelper {
+    // Construct the client credentials grant
+    private String CLIENT_SECRET = System.getenv("AZURE_APP_CLIENT_SECRET");
+    private String CLIENT_ID = System.getenv("AZURE_APP_CLIENT_ID");
+    private String TENANT = System.getenv("TENANT");
+    private String ENV = System.getenv("ENV");
+    private ClientID clientID = new ClientID(CLIENT_ID);
+    private Secret clientSecret = new Secret(CLIENT_SECRET);
+    private ClientAuthentication clientAuth = new ClientSecretBasic(clientID, clientSecret);
+    private Scope scope = new Scope("api://"+ENV+"-fss.navdig.statusholder/.default");
+
+
+
+
+
+    public AccessToken getAccessTokenForStatusholder() throws URISyntaxException, IOException, ParseException {
+
+        AuthorizationGrant clientGrant = new ClientCredentialsGrant();
+        /**
+
+        // The credentials to authenticate the client at the token endpoint
+        ClientID clientID = new ClientID("123");
+        Secret clientSecret = new Secret("secret");
+        ClientAuthentication clientAuth = new ClientSecretBasic(clientID, clientSecret);
+
+        // The request scope for the token (may be optional)
+        Scope scope = new Scope("read", "write");
+        **/
+        // The token endpoint
+        URI tokenEndpoint =  new URI("https://login.microsoftonline.com/"+TENANT+"/oauth2/v2.0/token");
+
+        // Make the token request
+        TokenRequest request = new TokenRequest(tokenEndpoint, clientAuth, clientGrant, scope);
+
+        TokenResponse response = TokenResponse.parse(request.toHTTPRequest().send());
+
+        if (!response.indicatesSuccess()) {
+            // We got an error response...
+            TokenErrorResponse errorResponse = response.toErrorResponse();
+        }
+
+        AccessTokenResponse successResponse = response.toSuccessResponse();
+
+        // Get the access token
+        AccessToken accessToken = successResponse.getTokens().getAccessToken();
+
+        return accessToken;
+    }
+}
