c3e7ee521c5afc8dcacd95d106c737704d4b89d7
larsLotNav <lars.august.lotsberg@nav.no>
2023-02-01 11:43:27 +0100

    Returns empty alertdo if not set

--- portal-infrastructure/src/main/java/no/nav/portal/infrastructure/AuthenticationFilter.java
+++ portal-infrastructure/src/main/java/no/nav/portal/infrastructure/AuthenticationFilter.java
@@ -32,7 +32,7 @@ public class AuthenticationFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        logger.info("Request to api with :" + ((HttpServletRequest) request).getRequestURI() );
+        logger.info("Request to api with uri: " + ((HttpServletRequest) request).getRequestURI() + "and method: "+ ((HttpServletRequest) request).getMethod());
         if(validateSwaggerAuthentication(request)){
             chain.doFilter(request, response);
             MDC.clear();
