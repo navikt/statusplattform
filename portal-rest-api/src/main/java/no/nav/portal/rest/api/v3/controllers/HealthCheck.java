32cc5315457fb923d779c4fc249eaa34c22a248b
larsLotNav <lars.august.lotsberg@nav.no>
2023-06-26 13:03:53 +0200

    Time convertion

--- portal-rest-api/src/main/java/no/nav/portal/rest/api/v3/controllers/HealthCheck.java
+++ portal-rest-api/src/main/java/no/nav/portal/rest/api/v3/controllers/HealthCheck.java
@@ -6,6 +6,12 @@ import org.actioncontroller.GET;
 import org.actioncontroller.json.JsonBody;
 import org.fluentjdbc.DbContext;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+
 
 public class HealthCheck {
     private final AreaRepository areaRepository;
@@ -21,9 +27,11 @@ public class HealthCheck {
     @JsonBody
     public String getDashboards() {
         boolean dbConnectionStatus = !areaRepository.retriveAllShallow().isEmpty();
+        ZonedDateTime lastUpdateGcp = recordRepository.getLatestGcpPollTime();
+        ZonedDateTime lastUpdateFss = recordRepository.getLatestFssPollTime();
         return "Status: OK, " +
-                "LastGcpPoll: " + recordRepository.getLatestGcpPollTime() +
-                ", LastFssPoll: " + recordRepository.getLatestFssPollTime()+
+                "LastGcpPoll: " + OffsetDateTime.of(lastUpdateGcp.toLocalDateTime(),lastUpdateGcp.getOffset()).truncatedTo(ChronoUnit.SECONDS) +
+                ", LastFssPoll: " + OffsetDateTime.of(lastUpdateFss.toLocalDateTime(),lastUpdateFss.getOffset()).truncatedTo(ChronoUnit.SECONDS) +
                 ", DbStatus: "+dbConnectionStatus;
     }
 
