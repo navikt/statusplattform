da957a77d417007339393c9a4c52a3e00a43cc20
M137316 <orlene.mitchell@nav.no>
2024-06-13 13:46:18 +0200

    refactoring of code to sum percentage uptime for a service opened 24/7 to cater for the majority of services. This is only a draft not yet tested.

--- core/src/main/java/nav/statusplattform/core/openingHours/ServicesUpTimeRenumerator.java
+++ core/src/main/java/nav/statusplattform/core/openingHours/ServicesUpTimeRenumerator.java
@@ -14,57 +14,11 @@ import static java.time.temporal.ChronoUnit.MINUTES;
 
 public class ServicesUpTimeRenumerator {
     static RecordRepository recordRepository;
-    public static final int MINUTES_IN_A_HOUR = 60;
-    private static Set<Integer> weekdaysList;
-
 
     public static long calculateUpTimeForService(UUID serviceId, LocalDate DateEntryFrom, LocalDate DateEntryTo, String rule) {
         String[] ruleParts = "[ ]".split(rule);
-        if (!isWeekDaysRuleApplicable(ruleParts[2])) {
-            throw new IllegalStateException("Arguments for weekdays must be of of numeric values, representing 1 for Monday to 7 for Sunday");
-        }
 
-        //ruleParts[3] represents the services opening hours
-        return checkAndGetCalculationForUpTimePercentage(serviceId, DateEntryFrom, DateEntryTo, ruleParts[3]);
-    }
-
-    private static boolean isWeekDaysRuleApplicable(String weekDayRule) {
-
-        //Checks for ?
-        if (weekDayRule.equals("?")) {
-            weekdaysList = Set.of(1, 2, 3, 4, 5, 6, 7);
-            return true;
-        }
-
-        //int dayOfWeekNumber = dateTimeEntry.getDayOfWeek().getValue();
-
-        weekdaysList = new HashSet<>();
-
-        //Checks for a singular week day in the month or for a range
-        String[] ruleParts = weekDayRule.split("[,]");
-
-        int lowerRange;
-        int upperRange;
-
-        //check the range/s for a weekday number match
-        for (String rulePart : ruleParts) {
-            if (rulePart.contains("-")) {
-                //checks weekday falls within a range
-                lowerRange = Integer.parseInt(rulePart.substring(0, 1));
-                upperRange = Integer.parseInt(rulePart.substring(2));
-                for (int i = lowerRange; i < upperRange; i++) {
-                    weekdaysList.add(i);
-                }
-            } else {
-                //checks weekday value matches a single weekday value
-                weekdaysList.add(Integer.parseInt(rulePart));
-            }
-        }
-        return !weekdaysList.isEmpty();
-    }
-
-
-    public static long checkAndGetCalculationForUpTimePercentage(UUID serviceId, LocalDate DateEntryFrom, LocalDate DateEntryTo, String openingHours) {
+        String openingHours = ruleParts[3];
 
         //checks for yyyy-MM-dd
         if (DateEntryFrom == null || DateEntryTo == null) {
@@ -91,100 +45,27 @@ public class ServicesUpTimeRenumerator {
             openingHours = "00:00-24:00";
         }
 
-        String[] ruleParts = openingHours.split("-");
+        String[] openingHoursParts = openingHours.split("-");
 
         //Obtain the time in hh:mm format
-        String[] ohStartTime = ruleParts[0].split(":"); //start time hours[0] and minutes[1]
-        String[] ohEndTime = ruleParts[1].split(":"); //end time hours[0] and minutes[1]
+        String[] ohStartTime = openingHoursParts[0].split(":"); //start time hours[0] and minutes[1]
+        String[] ohEndTime = openingHoursParts[1].split(":"); //end time hours[0] and minutes[1]
 
-        //Obtain time in LocalDateFormat
+        //Convert string to local date format to get  LocalDateFormat that can be used to obtain zone datetime
         LocalTime ohLocalTimeStart = LocalTime.of(Integer.parseInt(ohStartTime[0]), Integer.parseInt(ohStartTime[1]));
         LocalTime ohLocalTimeEnd = LocalTime.of(Integer.parseInt(ohEndTime[0]), Integer.parseInt(ohEndTime[1]));
 
-        //Obtain current tme of request
-        LocalTime currentTime = LocalTime.now();
 
         //Obtained zonedDateTime
-        ZonedDateTime zonedDateTimeFrom = ZonedDateTime.of(DateEntryFrom, ohLocalTimeStart, (ZoneId.of("Europe/Oslo")));
-        ZonedDateTime zonedDateTimeTo = ZonedDateTime.of(DateEntryTo, ohLocalTimeEnd, (ZoneId.of("Europe/Oslo")));
+        ZonedDateTime from = ZonedDateTime.of(DateEntryFrom, ohLocalTimeStart, (ZoneId.of("Europe/Oslo")));
+        ZonedDateTime to = ZonedDateTime.of(DateEntryTo, ohLocalTimeEnd, (ZoneId.of("Europe/Oslo")));
 
-        final int dailyOpeningHours = getDailyOpeningHours(ohStartTime, ohEndTime);
-
-        //Total opening hours in minutes
-        long totalOpeningTimeInMinutes = calculateTotalOpeningTime(DateEntryFrom, DateEntryTo, dailyOpeningHours, ohLocalTimeStart,
-                ohLocalTimeEnd, currentTime);
-
-
-        //Total down time for service in minutes
-        long totalDownTimeInMinutes = 10; //
-
-        /* Percentage uptime of service  is  the services uptime total - total down time total divided up time times by 100 */
-        return (totalOpeningTimeInMinutes - totalDownTimeInMinutes) / totalDownTimeInMinutes * 100;
-    }
-
-    public static long calculateTotalOpeningTime(LocalDate DateEntryFrom, LocalDate DateEntryTo, int dailyOpeningHours, LocalTime ohLocalTimeStart,
-                                                 LocalTime ohLocalTimeEnd, LocalTime currentTime) {
-        //Calculate total number of minutes in a period from fromDateEntry to toDateEntry
-        int allDaysBetween = (int) (ChronoUnit.DAYS.between(DateEntryFrom, DateEntryTo) + 1);
-        long allOpeningHourMinutes = IntStream.range(0, allDaysBetween)
-                .filter(i -> isWorkingDay(DateEntryFrom.plusDays(i)))
-                .count() * dailyOpeningHours;
-
-
-        long unlapsedTimeinCurrentDate = 0;
-
-        /*If the request includes the current date and falls within the services opening hours only the time
-         lapsed from opening hours start time is included as part of the calculation. The lapsed time is removed*/
-        if (DateEntryTo.isEqual(LocalDate.now())) { //check for input entryDate falls on same day as current date
-            //Check that time of request is after opening hours start but before opening hours end
-            if (currentTime.isAfter(ohLocalTimeStart) && currentTime.isBefore(ohLocalTimeEnd)) {
-                unlapsedTimeinCurrentDate = MINUTES.between(currentTime, ohLocalTimeEnd);
-            }
-        }
-
-        return allOpeningHourMinutes - unlapsedTimeinCurrentDate;
+        return calculatePercentageUptime(serviceId, from, to);
 
     }
 
 
-
-    private static int getDailyOpeningHours(String[] openingString, String[] closingString) {
-        LocalTime openingTime = LocalTime.of(Integer.parseInt(openingString[0]), Integer.parseInt(openingString[1]));
-        LocalTime closingTime = LocalTime.of(Integer.parseInt(closingString[0]), Integer.parseInt(closingString[1]));
-
-        int startHour = Integer.parseInt(openingString[0]);
-        int endHour = Integer.parseInt(closingString[0]);
-
-
-        //Check if opening hours start is greater than end time
-        if (openingTime.isAfter(closingTime)) {
-            throw new IllegalStateException("Opening hours end time must be greater than start time");
-        }
-
-        // hours [0] and mins [1]
-        int closingTimeMinutes = (int) ((Integer.parseInt(closingString[0]) * MINUTES_IN_A_HOUR) + Integer.parseInt(closingString[1]));
-        int openingTimeMinutes = (int) ((Integer.parseInt(openingString[0]) * MINUTES_IN_A_HOUR) + Integer.parseInt(openingString[1]));
-
-
-        //Total number of minutes in a opening times
-        int totalOpeningHours = closingTimeMinutes - openingTimeMinutes;
-        return totalOpeningHours;
-    }
-
-    public static boolean isWorkingDay(final LocalDate date) {
-        return weekdaysList.contains(date.getDayOfWeek().getValue());
-    }
-
-    static long getOpeningHoursInPeriod(ZonedDateTime d1, ZonedDateTime d2) {
-        return ChronoUnit.MINUTES.between(d1, d2);
-    }
-
-
-    static long getTimeDifference(ZonedDateTime d1, ZonedDateTime d2) {
-        return ChronoUnit.MINUTES.between(d1, d2);
-    }
-
-    public long calculatePercentageUptime(UUID serviceId, ZonedDateTime from, ZonedDateTime to) {
+    public static long calculatePercentageUptime(UUID serviceId, ZonedDateTime from, ZonedDateTime to) {
         // These records Have to be sorted in chrononlogical order
         List<RecordEntity> records = recordRepository.getRecordsInTimeSpan(serviceId, from, to);
 
@@ -196,12 +77,14 @@ public class ServicesUpTimeRenumerator {
         //Sum up (A) all the time  service has been UP, and all the time service should have been up
         for (RecordEntity currentRecord : records.subList(1, records.size())) {
             // Get the summarized (NOT AVERAGED) amount of minutes of uptime in this timespan that the openinghoursrule(s) expect
-            long expectedOpeningHoursTimeSpan = getOpeningHoursInPeriod(previousTimestamp, currentRecord.getCreated_at());
+            //Obtains the time difference between two periods
+            long expectedOpeningHoursTimeSpan = getTimeDifferenceInMinutes(previousTimestamp, currentRecord.getCreated_at());
             sumOfExpectedUptime += expectedOpeningHoursTimeSpan;
 
             // If the currentRecord is of uptime, record it
             if (currentRecord.getStatus() == ServiceStatus.OK) {
-                sumOfActualUptime += getTimeDifference(previousRecord.getCreated_at(), currentRecord.getCreated_at());
+                //Obtains the time difference
+                sumOfActualUptime += getTimeDifferenceInMinutes(previousRecord.getCreated_at(), currentRecord.getCreated_at());
             }
             // Prepare for next iteration of loop
             previousRecord = currentRecord;
@@ -209,16 +92,25 @@ public class ServicesUpTimeRenumerator {
 
         RecordEntity lastRecord = records.getLast();
         if (lastRecord.getCreated_at().isBefore(to)) {
-            long expectedOpeningHoursTimeSpan = getOpeningHoursInPeriod(lastRecord.getCreated_at(), to);
+            //Obtains the time difference between two periods
+            long expectedOpeningHoursTimeSpan = getTimeDifferenceInMinutes(lastRecord.getCreated_at(), to);
             sumOfExpectedUptime += expectedOpeningHoursTimeSpan;
 
             //If the current record is of uptime , record it
             if (lastRecord.getStatus() == ServiceStatus.OK) {
-                sumOfActualUptime += getTimeDifference(lastRecord.getCreated_at(), to);
+                //Obtains the time difference
+                sumOfActualUptime += getTimeDifferenceInMinutes(lastRecord.getCreated_at(), to);
             }
 
         }
         return (sumOfActualUptime / sumOfExpectedUptime) * 100; // Uptime percentage calculation
     }
+
+    static long getTimeDifferenceInMinutes(ZonedDateTime d1, ZonedDateTime d2) {
+        return ChronoUnit.MINUTES.between(d1, d2);
+    }
+
+
+
 
 }
