f4d83dc45229200c10a485d227526418c633821d
M137316 <orlene.mitchell@nav.no>
2024-06-20 15:31:55 +0200

    Updating code to use zonedDatetime  to account for anomalies such as Daylight Saving Time (DST) or other changes to time-keeping.

--- core/src/main/java/nav/statusplattform/core/openingHours/ServicesUpTimeRenumerator.java
+++ core/src/main/java/nav/statusplattform/core/openingHours/ServicesUpTimeRenumerator.java
@@ -13,10 +13,6 @@ import static java.time.LocalDate.*;
 
 public class ServicesUpTimeRenumerator {
 
-    private static LocalTime oHStartTime;
-    private static LocalTime oHEndTime;
-
-
     static RecordRepository recordRepository;
 
     public static long calculateUpTimeForService(UUID serviceId, LocalDate DateEntryFrom, LocalDate DateEntryTo, String rule) {
@@ -56,9 +52,8 @@ public class ServicesUpTimeRenumerator {
         String[] ohEndTime = openingHoursParts[1].split(":"); //end time hours[0] and minutes[1]
 
         //Convert string to local date format to get  LocalDateFormat that can be used to obtain zone datetime
-        oHStartTime = LocalTime.of(Integer.parseInt(ohStartTime[0]), Integer.parseInt(ohStartTime[1]));
-        oHEndTime = LocalTime.of(Integer.parseInt(ohEndTime[0]), Integer.parseInt(ohEndTime[1]));
-
+        LocalTime oHStartTime = LocalTime.of(Integer.parseInt(ohStartTime[0]), Integer.parseInt(ohStartTime[1]));
+        LocalTime oHEndTime = LocalTime.of(Integer.parseInt(ohEndTime[0]), Integer.parseInt(ohEndTime[1]));
 
         //Obtained zonedDateTime
         ZonedDateTime from = ZonedDateTime.of(DateEntryFrom, oHStartTime, (ZoneId.of("Europe/Oslo")));
@@ -73,41 +68,38 @@ public class ServicesUpTimeRenumerator {
 
         List<RecordEntity> records = recordRepository.getRecordsInTimeSpan(serviceId, from, to);
 
-        long sumOfActualUptime = 0; //Represents the total uptime for service eg service status equals
+        long sumOfActualUptime = 0; //Represents the total uptime for service for example,  service status equals
         long sumOfExpectedUptime = 0; //Represents the total time
 
         RecordEntity previousRecord = records.getFirst();
-        ZonedDateTime previousTimestamp = previousRecord.getCreated_at();
 
         //Sum up (A) all the time  service has been UP, and all the time service should have been up
         for (RecordEntity currentRecord : records.subList(1, records.size())) {
-            //Convert Zoned date time to LocalDateTime to obtain date
-
-            ZonedDateTime currentTimeStamp = currentRecord.getCreated_at();
-            LocalDateTime LdtCurrentTimeStamp = currentTimeStamp.toLocalDateTime();
-            LocalDateTime ldtPreviousTimestamp = previousTimestamp.toLocalDateTime();
-
 
             // Get the summarized amount of minutes of uptime specified within the timespan that the opening hours rule(s) expect
             //Obtains the time difference between two periods
             long expectedOpeningHoursTimeSpan;
 
-            int allDaysBetween = (int) (ChronoUnit.DAYS.between(ldtPreviousTimestamp, LdtCurrentTimeStamp) + 1);
+            int allDaysBetween = (int) (ChronoUnit.DAYS.between(from, to) + 1);
 
             expectedOpeningHoursTimeSpan = IntStream.range(0, allDaysBetween)
-                    .count() * getOpeningHoursInMinutes(oHStartTime, oHEndTime);
+                    .count() * getOpeningHoursInMinutes(from, to);
 
 
-            //Remove any redundant time from opening hours start time from previous records actual start time
+            /*Remove any redundant time from opening hours start time from previous records actual start time as long
+            as it within the opening hours specified by the rule */
             long trailingRedundantMinutes = 0;
-            if (previousTimestamp.toLocalTime().isAfter(oHStartTime)) {
-                trailingRedundantMinutes = getOpeningHoursInMinutes(oHStartTime, previousTimestamp.toLocalTime());
+            if (previousRecord.getCreated_at().toLocalTime().isAfter(from.toLocalTime())
+                    && previousRecord.getCreated_at().toLocalTime().isBefore(to.toLocalTime())) {
+                trailingRedundantMinutes = getOpeningHoursInMinutes(from, previousRecord.getCreated_at());
             }
 
-            //Remove any redundant time from opening hours end time from current record actual end time
+            /*Remove any redundant time from opening hours end time from current record actual end time
+            as long as it within the opening hours specified by the rule */
             long leadingRedundantMinutes = 0;
-            if (currentTimeStamp.toLocalTime().isBefore(oHEndTime)) {
-                leadingRedundantMinutes = getOpeningHoursInMinutes(currentTimeStamp.toLocalTime(), oHEndTime);
+            if (currentRecord.getCreated_at().toLocalTime().isBefore(to.toLocalTime()) &&
+                    currentRecord.getCreated_at().toLocalTime().isAfter(from.toLocalTime())) {
+                leadingRedundantMinutes = getOpeningHoursInMinutes(currentRecord.getCreated_at(), to);
             }
 
             //total time between two previous and current records
@@ -126,7 +118,9 @@ public class ServicesUpTimeRenumerator {
         return (sumOfActualUptime / sumOfExpectedUptime) * 100; // Uptime percentage calculation
     }
 
-    private static long getOpeningHoursInMinutes(LocalTime startTime, LocalTime endTime) {
-        return ChronoUnit.MINUTES.between(startTime, endTime);
+    private static long getOpeningHoursInMinutes(ZonedDateTime from, ZonedDateTime to) {
+        LocalDateTime ltdFrom = from.toLocalDateTime();
+        LocalDateTime ltdTo = to.toLocalDateTime();
+        return ChronoUnit.MINUTES.between(ltdFrom.toLocalTime(), ltdTo.toLocalTime());
     }
 }
