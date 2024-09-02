adb90e9ab679dd0cfeb23ea1ffa55ffc419a6bec
M137316 <orlene.mitchell@nav.no>
2024-09-02 11:59:57 +0200

    Added a new message for clarity

--- core/src/main/java/nav/statusplattform/core/openingHours/UpTimeCalculator.java
+++ core/src/main/java/nav/statusplattform/core/openingHours/UpTimeCalculator.java
@@ -19,9 +19,14 @@ public class UpTimeCalculator {
         this.openingHoursRepository = openingHoursRepository;
     }
 
+    /*Calculation of service's actual and expected uptimes:
+     Obtain the opening hours group of business opening times and get the records storing information about a
+     service's availability for a period requested by the user. For each record, apply the opening times and create a
+     chronology of the service's availability events. Finally, sum up the event times to calculate a service's
+     actual and expected uptimes.*/
     public UpTimeTotal calculateUpTimeForService(UUID serviceId, TimeSpan timeSpan) throws IllegalStateException {
         OpeningHoursGroup openingHoursGroup = openingHoursRepository.getOHGroupForService(serviceId)
-                .orElseThrow(() -> new IllegalStateException("Didn´t find any opening hours for serviceId = %s".formatted(serviceId)));
+                .orElseThrow(() -> new IllegalStateException("No timespan assigned for serviceId = %s".formatted(serviceId)));
 
 
         Records records = Records.fromRecordEntities(recordRepository.getRecordsInTimeSpan(serviceId, timeSpan.from(), timeSpan.to()), timeSpan);
